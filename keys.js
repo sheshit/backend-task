09562b28ab1b039835192b96b3a80b5d150e4359
sheshit <sheshitkarthikeya237@gmail.com>
2019-06-10 15:35:14 +0530

    access keys removed

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-  iam_access_id: 'AKIA5EAMOI3C7ZCLI7VE',
-  iam_secret: '+xX3+MsthwhRLrzc+kiJZFNDZkCUOW3NIGLRhc2i',
+  iam_access_id: 'dummy_key',
+  iam_secret: 'dummy_key',
   CustomerDB_URL:
-    'mongodb+srv://sheshit:helloworld@cluster0-vdkoe.mongodb.net/customer?retryWrites=true&w=majority'
+    'mongodb+srv://sheshit:dummy_password@cluster0-vdkoe.mongodb.net/customer?retryWrites=true&w=majority'
 };
